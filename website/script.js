62ecb01bdedd78523262d5d016cffc64c65630aa
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2024-09-10 18:27:48 +0200

    update bio picture to scale with name or profession

--- website/script.js
+++ website/script.js
@@ -113,10 +113,24 @@ function populateExperience() {
     });
 };
 
+function getWidestIntroductionElement() {
+  const name = document.querySelector('.introduction__name');
+  const profession = document.querySelector('.introduction__profession');
+  const workplace = document.querySelector('#if__introduction__workplace');
+  const nameWidth = name.getBoundingClientRect().width;
+  const professionWidth = profession.getBoundingClientRect().width;
+  const workplaceWidth = workplace !== null ? workplace.getBoundingClientRect().width : 0;
+  return nameWidth > professionWidth + workplaceWidth ? nameWidth : professionWidth + workplaceWidth;
+}
+
 document.getElementById('if__biography__about').addEventListener('click', function(event) {
   event.preventDefault();
   const hiddenText = document.querySelector('.biography__about');
   const hiddenPicture = document.querySelector('.biography__picture');
+  const imgElement = document.querySelector('.biography__picture img');
+  imgHeight = getWidestIntroductionElement() + "px";
+  imgElement.style.height = imgHeight;
+  imgElement.style.width = imgHeight;
 
   if (hiddenText.style.maxHeight) {
       hiddenText.style.maxHeight = null;
