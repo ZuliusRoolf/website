ff4b1c216eed2f6736bc01c7eda4c91049cb9228
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2024-10-01 19:42:43 +0200

    update project selection (works on mobile 100%)

--- website/scripts/handle_projects.js
+++ website/scripts/handle_projects.js
@@ -1,9 +1,10 @@
 export function projectsAddEventListeners(document) {
 
     const portfolioContainer = document.getElementById('portfolio__container');
-    const previewContainer = document.getElementById('portfolio__project__container');
-    var selectedProject = null;
-    var hoveredProject = null;
+    const selectedContainer = document.getElementById('project__container');
+    const hoveredContainer = document.getElementById('hover__project__container');
+    var selectedButton = null;
+    var hoveredButton = null;
 
     portfolioContainer.addEventListener('mouseover', function (event) {
         if (window.innerWidth < 768) {
@@ -14,14 +15,8 @@ export function projectsAddEventListeners(document) {
             if (button.contains(event.relatedTarget))
                 // Ignore mouse enter on child elements
                 return;
-
-            if (selectedProject && selectedProject !== button) {
-                // Temporarily show other project
-                showPreview(button);
-            } else {
-                // Show the hovered content
-                showPreview(button, selectedProject === button);
-            }
+            var project = getProjectContent(button);
+            showHoveredProject(project);
         }
     }, true);
 
@@ -34,33 +29,27 @@ export function projectsAddEventListeners(document) {
             if (button.contains(event.relatedTarget))
                 // Ignore mouse leave on child elements
                 return;
-
-            if (selectedProject) {
-                // Restore the selected content
-                showPreview(selectedProject, true);
-            } else {
-                // Hide the preview area
-                hidePreview();
-            }
+            hideHoveredProject();
         }
     }, true);
 
     portfolioContainer.addEventListener('click', function (event) {
         var button = event.target.closest('.project__button');
         if (button) {
-            if (selectedProject === button) {
-                // Deselect the project
-                selectedProject = null;
-                hidePreview();
-            } else {
+            if (window.innerWidth < 768) {
                 // Select the project
-                selectedProject = button;
-                showPreview(button, true);
+                showSelectedProject(button);
+                return;
+            }
+            if (selectedButton === button) {
+                // Deselect the project
+                selectedButton = null;
+                hidePreview();
             }
         }
     }, true);
 
-    previewContainer.addEventListener('click', function (event) {
+    selectedContainer.addEventListener('click', function (event) {
         if (window.innerWidth >= 768) {
             return;
         }
@@ -68,8 +57,7 @@ export function projectsAddEventListeners(document) {
             return;
         }
         // Deselect the project
-        selectedProject = null;
-        hidePreview();
+        hideSelectedProject();
     });
 
     // Function to get the template content of a button
@@ -87,75 +75,52 @@ export function projectsAddEventListeners(document) {
         }
         return null;
     }
-    
 
-    // Function to show the preview
-    function showPreview(button, isSelected) {
-        var content = getProjectContent(button);
-        if (content) {
-            var currentContent = previewContainer.querySelector('.portfolio__project');
-            var currentTemplateId = currentContent ? currentContent.getAttribute('data-template-id') : null;
-            var newTemplateId = content.getAttribute('data-template-id');
-    
-            // Check if the content is already displayed and if the preview is visible
-            if (currentTemplateId === newTemplateId && previewContainer.classList.contains('project__container--show')) {
-                if (isSelected) {
-                    previewContainer.querySelector('.project__content__selected').classList.add('project__content__selected--show');
-                }
-                return;
-            }
-    
-            // Cancel any ongoing hide animations
-            if (hideTransitionEndHandler) {
-                previewContainer.removeEventListener('transitionend', hideTransitionEndHandler);
-                hideTransitionEndHandler = null;
-                previewContainer.classList.remove('project__container--hide');
-            }
-    
-            // Clear the preview area
-            previewContainer.innerHTML = '';
-    
-            // Append the new content
-            previewContainer.appendChild(content);
-            previewContainer.classList.add('project__container--show');
-            previewContainer.querySelector('.project__content__hover').classList.add('project__content__hover--show');
-            if (isSelected) {
-                previewContainer.querySelector('.project__content__selected').classList.add('project__content__selected--show');
-            }
-        }
-    }
-    
-    
-    
-
-    // Function to hide the preview
-    var hideTransitionEndHandler = null;
-    function hidePreview() {
-        const content = previewContainer.querySelector('.portfolio__project');
-        if (!content) {
+    function showSelectedProject(button) {
+        if (selectedButton === button) {
             return;
         }
-    
-        // Add a class to trigger fade-out animation
-        previewContainer.classList.remove('project__container--show');
-        previewContainer.classList.add('project__container--hide');
-        previewContainer.querySelector('.project__content__selected').classList.remove('project__content__selected--show');
-    
-        // Listen for transition end to remove content
-        hideTransitionEndHandler = function () {
-            // Remove the event listener
-            previewContainer.removeEventListener('transitionend', hideTransitionEndHandler);
-            hideTransitionEndHandler = null;
-    
-            // Remove the content
-            content.remove();
-    
-            // Remove the hide class
-            previewContainer.classList.remove('project__container--hide');
-        };
-        previewContainer.addEventListener('transitionend', hideTransitionEndHandler);
+        if (window.innerWidth < 768) {
+            selectedButton = button;
+            const project = getProjectContent(button);
+            selectedContainer.appendChild(project);
+            void project.offsetWidth;
+            project.classList.add('enter');
+            project.querySelector('.project__content__selected').classList.add('enter');
+            selectedContainer.classList.add('enter');
+            return;
+        }
+    }
+
+    function hideSelectedProject() {
+        if (selectedButton === null) {
+            return;
+        }
+        selectedButton = null;
+        const project = selectedContainer.querySelector('.portfolio__project');
+        if (project === null) {
+            return;
+        }
+        project.classList.remove('enter');
+        project.classList.add('exit');
+        project.addEventListener('transitionend', onTransitionEnd);
+        selectedContainer.classList.remove('enter');
+        selectedContainer.classList.add('exit');
+
+        function onTransitionEnd(event) {
+            if (event.propertyName === 'opacity') {
+                project.removeEventListener('transitionend', onTransitionEnd);
+                selectedContainer.removeChild(project);
+                selectedContainer.classList.remove('exit');
+            }
+        }
+    }
+
+    function showHoveredProject(button) {
+        return;
     }
     
-    
-
+    function hideHoveredProject(){
+        return;
+    }
 }
