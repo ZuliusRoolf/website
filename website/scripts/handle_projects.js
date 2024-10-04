843c738439c6e2f93c2310eef3bbb468a0783096
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2024-10-04 22:30:07 +0200

    update project selection (works on tablet 100%)

--- website/scripts/handle_projects.js
+++ website/scripts/handle_projects.js
@@ -3,8 +3,6 @@ export function projectsAddEventListeners(document) {
     const portfolioContainer = document.getElementById('portfolio__container');
     const selectedContainer = document.getElementById('project__container');
     const hoveredContainer = document.getElementById('hover__project__container');
-    var selectedButton = null;
-    var hoveredButton = null;
 
     portfolioContainer.addEventListener('mouseover', function (event) {
         if (window.innerWidth < 768) {
@@ -32,31 +30,22 @@ export function projectsAddEventListeners(document) {
         }
     }, true);
 
-    portfolioContainer.addEventListener('click', function (event) {
+    portfolioContainer.addEventListener('pointerdown', function (event) {
         var button = event.target.closest('.project__button');
         if (button) {
-            if (window.innerWidth < 768) {
-                // Select the project
-                showSelectedProject(button);
-                return;
-            }
-            if (selectedButton === button) {
-                // Deselect the project
-                selectedButton = null;
-                hidePreview();
-            }
+            showSelectedProject(button);
         }
     }, true);
 
-    selectedContainer.addEventListener('click', function (event) {
+    selectedContainer.addEventListener('pointerdown', function (event) {
         if (window.innerWidth >= 768) {
             return;
         }
         if (event.target.closest('.portfolio__project')) {
             return;
         }
-        // Deselect the project
-        hideSelectedProject();
+        // Deselect the project in mobile view
+        hideAllSelectedProjects();
     });
 
     // Function to get the template content of a button
@@ -75,49 +64,134 @@ export function projectsAddEventListeners(document) {
         return null;
     }
 
+    function getProjectInstance(button, container) {
+        const selector = '.portfolio__project[data-template-id="' + button.getAttribute('data-template-id') + '"]';
+        return container.querySelector(selector);
+    }
+
     function showSelectedProject(button) {
-        if (selectedButton === button) {
-            return;
-        }
-        if (window.innerWidth < 768) {
-            selectedButton = button;
+        const selectedProject = getProjectInstance(button, selectedContainer);
+        const hoveredProject = getProjectInstance(button, hoveredContainer);
+
+        // Mobile view (no hover effect)
+        if (hoveredProject === null) {
+            if (selectedContainer.querySelector('.portfolio__project')) {
+                hideAllSelectedProjects();
+            }
+            if (selectedProject !== null) {
+                return;
+            }
             const project = getProjectContent(button);
             selectedContainer.appendChild(project);
             void project.offsetWidth;
             project.classList.add('enter');
             project.querySelector('.project__content__selected').classList.add('enter');
+            selectedContainer.classList.remove('exit');
             selectedContainer.classList.add('enter');
             return;
         }
+
+
+        // PSEUDO CODE
+        // Check if the click is supposed to select or deselect
+        // if select then deselect current selection
+        // move project from hovered to selected
+        // apply animations
+        // if deselect then move project from selected to hovered
+        // apply animations
+
+        // Deselect the project
+        if (selectedProject !== null) {
+            hideAllSelectedProjects();
+            return;
+        }
+
+        // Select the project
+        if (selectedProject === null) {
+            return;
+        }
+
+
+
+
+        const selectedProjectList = selectedContainer.querySelectorAll('.portfolio__project');
+        selectedProjectList.forEach(project => {
+            project.classList.remove('enter');
+            project.classList.add('exit');
+            console.log('exit applied on ' + project.getAttribute('data-template-id'));
+
+        });
+
+        console.log(hoveredProject);
+        console.log(selectedProject);
+
+
+
+        if (hoveredProject !== null) {
+            if (selectedProject !== null)
+                selectedContainer.removeChild(selectedProject);
+            selectedContainer.classList.add('enter');
+            selectedContainer.classList.remove('exit');
+            selectedContainer.appendChild(hoveredProject);
+            hoveredContainer.classList.remove('enter');
+            hoveredContainer.classList.remove('exit');
+            // Initilize so that it can utilize animations
+            const projectContent = hoveredProject.querySelector('.project__content__selected');
+            void projectContent.offsetWidth;
+            projectContent.classList.add('enter');
+            projectContent.classList.remove('exit');
+            return;
+        }
+
+        if (selectedProject !== null) {
+            selectedContainer.classList.remove('enter');
+            selectedContainer.classList.add('exit');
+            hoveredContainer.appendChild(selectedProject);
+            hoveredContainer.classList.add('enter');
+            hoveredContainer.classList.remove('exit');
+            // Initilize so that it can utilize animations
+            const projectContent = selectedProject.querySelector('.project__content__selected');
+            void projectContent.offsetWidth;
+            projectContent.classList.add('enter');
+            projectContent.classList.remove('exit');
+        }
+
+
     }
 
-    function hideSelectedProject() {
-        if (selectedButton === null) {
-            return;
-        }
-        selectedButton = null;
-        const project = selectedContainer.querySelector('.portfolio__project');
-        if (project === null) {
-            return;
-        }
-        project.classList.remove('enter');
-        project.classList.add('exit');
-        project.addEventListener('transitionend', onTransitionEnd);
+    function hideAllSelectedProjects() {
+        const projectList = selectedContainer.querySelectorAll('.portfolio__project');
+        projectList.forEach(project => {
+            project.classList.remove('enter');
+            project.classList.add('exit');
+            project.addEventListener('transitionend', onTransitionEnd);
+
+        });
+
         selectedContainer.classList.remove('enter');
         selectedContainer.classList.add('exit');
 
         function onTransitionEnd(event) {
             if (event.propertyName === 'opacity') {
-                project.removeEventListener('transitionend', onTransitionEnd);
-                selectedContainer.removeChild(project);
-                selectedContainer.classList.remove('exit');
+                event.target.removeEventListener('transitionend', onTransitionEnd);
+                if (event.target.classList.contains('exit')) {
+                    if (event.target.parentNode) {
+                        event.target.parentNode.removeChild(event.target);
+                    }
+                }
+                if (selectedContainer.querySelector('.portfolio__project') === null) {
+                    selectedContainer.classList.remove('exit');
+                    selectedContainer.classList.remove('enter');
+                }
             }
         }
     }
 
     function showHoveredProject(button) {
-        const selector = '.portfolio__project[data-template-id="' + button.getAttribute('data-template-id') + '"]';
-        var project = hoveredContainer.querySelector(selector);
+        var project = getProjectInstance(button, hoveredContainer);
+
+        if (getProjectInstance(button, selectedContainer) !== null)
+            return;
         // var project = hasHovered(button);
         if (project !== null) {
             project.classList.add('enter');
@@ -134,19 +208,17 @@ export function projectsAddEventListeners(document) {
         hoveredContainer.classList.remove('exit');
         hoveredContainer.classList.add('enter');
         return;
-    
-    
+
+
     }
 
-    function hideHoveredProject(button){
-        const selector = '.portfolio__project[data-template-id="' + button.getAttribute('data-template-id') + '"]';
-        const project = hoveredContainer.querySelector(selector);
-        
+    function hideHoveredProject(button) {
+        const project = getProjectInstance(button, hoveredContainer);
+
         if (project === null) {
             return;
         }
-        console.log(project);
-        
+
         project.classList.remove('enter');
         project.classList.add('exit');
         project.addEventListener('transitionend', onTransitionEnd);
